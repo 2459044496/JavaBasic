893ba6b313bbe7be1dc527232a0ed599667413a6
CuiPer <2459044496@qq.com>
2021-12-19 20:43:17 +0800

    Java核心技术项目初始化及附带源码

---
+++ src/com/cui/corejava10/v1ch03/FirstSample/FirstSample.java
@@ -0,0 +1,7 @@
+package com.cui.corejava10.v1ch03.FirstSample;
+
+public class FirstSample {
+   public static void main(String[] args) {
+      System.out.println("We will not use 'Hello, World!'");
+   }
+}
